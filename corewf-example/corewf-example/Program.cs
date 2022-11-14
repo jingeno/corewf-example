f34c5c0b485366dde7e165ce282734db75300ac2
Joe Ingeno <joe.ingeno@protonmail.com>
2022-11-14 16:37:55 -0500

    Add files via upload

---
+++ corewf-example/corewf-example/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Activities.XamlIntegration;
+
+Console.WriteLine("Loading workflow...");
+string xaml = File.ReadAllText("WorkflowExample.xaml");
+var helloWorldActivity = ActivityXamlServices.Load(new StringReader(xaml));
+
+Console.WriteLine("Invoking workflow...");
+System.Activities.WorkflowInvoker.Invoke(helloWorldActivity);
+
+Console.ReadLine();
